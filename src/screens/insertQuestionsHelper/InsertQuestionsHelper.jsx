901db695d4e7de62fd09b910e02317e2d6e7b957
Juan M <juan@examplemail.com>
2025-03-22 15:01:25 -0400

    Improve insert questions helper

--- src/screens/insertQuestionsHelper/InsertQuestionsHelper.jsx
+++ src/screens/insertQuestionsHelper/InsertQuestionsHelper.jsx
@@ -31,10 +31,39 @@ const defaultItems = [1,2,3,4,5].map(index => ({
 
 const tomorrowsDate = time.getTomorrowDateWithoutTimeString();
 
+const QuickDatePicker = ({setRelevantDate}) => {
+  const dates = [
+    {
+      label: 'Today',
+      date: time.getDateWithoutTimeString()
+    },
+    {
+      label: 'Tomorrow',
+      date: time.getTomorrowDateWithoutTimeString()
+    },
+    {
+      label: 'Day After Tomorrow',
+      date: (new Date(time.getCurrentUtcDateTimeMinus5Hours().getTime() + 2 * 24 * 60 * 60 * 1000)).toISOString().split('T')[0]
+    }
+  ]
+
+  return (
+    <div className="mb-4">
+      {dates.map((date, index) => (
+        <button key={index} onClick={() => setRelevantDate(date.date)} className="mr-2 p-1 px-4 bg-secondary text-white text-xs rounded-full cursor-pointer">
+          {date.label}
+        </button>
+      ))}
+    </div>
+  )
+}
+
 const InsertQuestionsHelper = () => {
-  const [relevantDate, setRelevantDate] = useState(tomorrowsDate);
   const [error, setError] = useState(null);
   const [items, setItems] = useState(defaultItems);
+  const [indexFocused, setIndexFocused] = useState(null);
+  const [relevantDate, setRelevantDate] = useState(tomorrowsDate);
+  const textareaRows = { focused: 12, unfocused: 4 };
 
   useEffect(() => {
     supabase.getRelevantQuestionByDate({date: relevantDate}).then(question => {
@@ -91,6 +120,8 @@ const InsertQuestionsHelper = () => {
       <h1 className="text-2xl font-bold mb-4">Insert Questions Helper</h1>
 
       {/* Date picker */}
+      <QuickDatePicker setRelevantDate={setRelevantDate} />
+
       <input type="date" value={relevantDate} onChange={({target}) => setRelevantDate(target.value)} className="p-2 bg-white border border-gray-300 w-full" />
 
       {/* Error message */}
@@ -102,101 +133,112 @@ const InsertQuestionsHelper = () => {
 
       {/* Inputs for category and then questino text, for questions 1 through 5 */}
       <div className="mt-8 grid grid-cols-1 md:grid-cols-3 gap-4">
+
         {Array.from({ length: 5 }, (_, index) => (
-          <div key={index} className="flex flex-wrap p-4-md rounded-md shadow-md bg-white p-4">
-            <div className="mb-2 w-[66%] flex flex-col">
-              <span className="font-semibold leading-none">Category</span>
-
-              <input
-                type="text"
-                value={items[index].category}
-                onChange={({target}) => editItem({index, key: 'category', value: target.value})}
-                className="px-2 py-0 border border-gray-300"
-              />
+          <div key={index}>
+            <div className="mb-2 text-3xl font-bold leading-none">
+              {index + 1}
             </div>
 
-            <div className="mb-2 w-[33%] flex flex-col">
-              <span className="font-semibold leading-none">Emoji</span>
-
-              <input
-                type="text"
-                value={items[index].emoji}
-                onChange={({target}) => editItem({index, key: 'emoji', value: target.value})}
-                className="px-2 py-0 border border-gray-300"
-              />
-            </div>
-
-            <div className="mb-2 w-full flex flex-col">
-              <span className="font-semibold leading-none">Question</span>
-
-              <textarea
-                rows={4}
-                value={items[index].text}
-                onChange={({target}) => editItem({index, key: 'text', value: target.value})}
-                className="px-2 py-0 border border-gray-300 leading-none"
-              ></textarea>
-            </div>
-
-            {/* Three text inputs for the 3 possible answers to the question */}
-            {Array.from({ length: 3 }, (_, answerIndex) => (
-              <div key={answerIndex} className="mb-2 font-semibold w-full">
-                {/* Label and checkbox to mark answer as correct */}
-                <span className="leading-none">Option {answerIndex + 1}</span>
-                <span className="ml-2 leading-none">Correct</span>
-                <input
-                  type="checkbox"
-                  className="ml-2 w-5 h-5 text-accent1 border-gray-300 focus:ring-accent1 focus:ring-2"
-                  checked={items[index].options[answerIndex].correct}
-                  onChange={() => markCorrectAnswer(index, answerIndex)}
-                />
+            <div className="flex flex-wrap p-4-md rounded-md shadow-md bg-white p-4">
+              <div className="mb-2 w-[66%] flex flex-col">
+                <span className="font-semibold leading-none">Category</span>
 
                 <input
                   type="text"
-                  value={items[index].options[answerIndex].value}
-                  onChange={({target}) => editItemOption({index, optionIndex: answerIndex, key: 'value', value: target.value})}
+                  value={items[index].category}
+                  onChange={({target}) => editItem({index, key: 'category', value: target.value})}
+                  className="px-2 py-0 border border-gray-300"
+                />
+              </div>
+
+              <div className="mb-2 w-[33%] flex flex-col">
+                <span className="font-semibold leading-none">Emoji</span>
+
+                <input
+                  type="text"
+                  value={items[index].emoji}
+                  onChange={({target}) => editItem({index, key: 'emoji', value: target.value})}
+                  className="px-2 py-0 border border-gray-300"
+                />
+              </div>
+
+              <div className="mb-2 w-full flex flex-col">
+                <span className="font-semibold leading-none">Question</span>
+
+                <textarea
+                  rows={indexFocused === index ? textareaRows.focused : textareaRows.unfocused}
+                  value={items[index].text}
+                  onChange={({target}) => editItem({index, key: 'text', value: target.value})}
+                  onFocus={() => setIndexFocused(index)}
+                  onBlur={() => setIndexFocused(null)}
+                  className="px-2 py-0 border border-gray-300 leading-none"
+                ></textarea>
+              </div>
+
+              {/* Three text inputs for the 3 possible answers to the question */}
+              {Array.from({ length: 3 }, (_, answerIndex) => (
+                <div key={answerIndex} className="mb-2 font-semibold w-full">
+                  {/* Label and checkbox to mark answer as correct */}
+                  <span className="leading-none">Option {answerIndex + 1}</span>
+                  <span className="ml-2 leading-none">Correct</span>
+                  <input
+                    type="checkbox"
+                    className="ml-2 w-5 h-5 text-accent1 border-gray-300 focus:ring-accent1 focus:ring-2"
+                    checked={items[index].options[answerIndex].correct}
+                    onChange={() => markCorrectAnswer(index, answerIndex)}
+                  />
+
+                  <input
+                    type="text"
+                    value={items[index].options[answerIndex].value}
+                    onChange={({target}) => editItemOption({index, optionIndex: answerIndex, key: 'value', value: target.value})}
+                    className="px-2 py-0 border border-gray-300 w-full"
+                  />
+                </div>
+              ))}
+
+              <div className="mb-2 w-full flex flex-col">
+                <span className="font-semibold leading-none">News Article URL</span>
+                <input
+                  type="text"
+                  value={items[index].newsArticleUrl}
+                  onChange={({target}) => editItem({index, key: 'newsArticleUrl', value: target.value})}
                   className="px-2 py-0 border border-gray-300 w-full"
                 />
               </div>
-            ))}
 
-            <div className="mb-2 w-full flex flex-col">
-              <span className="font-semibold leading-none">News Article URL</span>
-              <input
-                type="text"
-                value={items[index].newsArticleUrl}
-                onChange={({target}) => editItem({index, key: 'newsArticleUrl', value: target.value})}
-                className="px-2 py-0 border border-gray-300 w-full"
-              />
-            </div>
+              <div className="mb-2 w-full flex flex-col">
+                <span className="font-semibold leading-none">Video Embed URL</span>
+                <input
+                  type="text"
+                  value={items[index].videoEmbed}
+                  onChange={({target}) => editItem({index, key: 'videoEmbed', value: target.value})}
+                  className="px-2 py-0 border border-gray-300 w-full"
+                />
+              </div>
 
-            <div className="mb-2 w-full flex flex-col">
-              <span className="font-semibold leading-none">Video Embed URL</span>
-              <input
-                type="text"
-                value={items[index].videoEmbed}
-                onChange={({target}) => editItem({index, key: 'videoEmbed', value: target.value})}
-                className="px-2 py-0 border border-gray-300 w-full"
-              />
-            </div>
+              <div className="mb-2 w-full flex flex-col">
+                <span className="font-semibold leading-none">Image URL</span>
+                <input
+                  type="text"
+                  value={items[index].imageUrl}
+                  onChange={({target}) => editItem({index, key: 'imageUrl', value: target.value})}
+                  className="px-2 py-0 border border-gray-300 w-full"
+                />
+              </div>
 
-            <div className="mb-2 w-full flex flex-col">
-              <span className="font-semibold leading-none">Image URL</span>
-              <input
-                type="text"
-                value={items[index].imageUrl}
-                onChange={({target}) => editItem({index, key: 'imageUrl', value: target.value})}
-                className="px-2 py-0 border border-gray-300 w-full"
-              />
-            </div>
-
-            <div className="mb-2 w-full flex flex-col">
-              <span className="font-semibold leading-none">Expanded Info</span>
-              <textarea
-                rows={4}
-                value={items[index].expandedInfo}
-                onChange={({target}) => editItem({index, key: 'expandedInfo', value: target.value})}
-                className="px-2 py-0 border border-gray-300 w-full"
-              />
+              <div className="mb-2 w-full flex flex-col">
+                <span className="font-semibold leading-none">Expanded Info</span>
+                <textarea
+                  rows={indexFocused === index ? textareaRows.focused : textareaRows.unfocused}
+                  value={items[index].expandedInfo}
+                  onChange={({target}) => editItem({index, key: 'expandedInfo', value: target.value})}
+                  onFocus={() => setIndexFocused(index)}
+                  onBlur={() => setIndexFocused(null)}
+                  className="px-2 py-0 border border-gray-300 w-full leading-none"
+                />
+              </div>
             </div>
           </div>
         ))}
